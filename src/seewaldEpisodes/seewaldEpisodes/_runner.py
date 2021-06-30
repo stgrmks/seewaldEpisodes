f17130e3d77df2ca14442f28011f951c33f371f9
Markus Steger <m.steger@pm.me>
2021-06-30 14:21:21 +0200

    overwrite settings with method parameters

--- src/seewaldEpisodes/seewaldEpisodes/_runner.py
+++ src/seewaldEpisodes/seewaldEpisodes/_runner.py
@@ -7,9 +7,10 @@ from twisted.internet import reactor
 from twisted.internet.defer import Deferred
 
 
-def get_episode():
+def get_episode(**custom_settings):
     settings: Settings = Settings()
     settings.setmodule(my_settings)
+    settings.update(custom_settings)
     runner: CrawlerRunner = CrawlerRunner(settings=settings)
     d: Deferred = runner.crawl(crawler_or_spidercls=SeewaldEpisodes)
     d.addBoth(lambda _: reactor.stop())
