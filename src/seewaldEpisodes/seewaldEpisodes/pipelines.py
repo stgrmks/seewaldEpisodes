626c6e139fd6751aaaae39dd7c7288c1084fc4c8
Markus Steger <m.steger@pm.me>
2021-06-30 14:15:03 +0200

    adding file pipeline

--- src/seewaldEpisodes/seewaldEpisodes/pipelines.py
+++ src/seewaldEpisodes/seewaldEpisodes/pipelines.py
@@ -6,10 +6,11 @@ from tqdm import tqdm
 from seewaldEpisodes.seewaldEpisodes.items import SeewaldepisodesItem
 
 
-class SeewaldepisodesPipeline:
+class SlackPipeline:
     def process_item(self, item: SeewaldepisodesItem, _):
         content: bytes = b""
         with get(url=item.get("url"), stream=True) as r:
+            r.raise_for_status()
             data_chunk: bytes
             for data_chunk in tqdm(r.iter_content(chunk_size=4096)):
                 content += data_chunk
@@ -30,4 +31,14 @@ class SeewaldepisodesPipeline:
             raise ValueError(
                 f"Request error! Status-Code: {response.status_code} Response:{response.text}"
             )
+        return item
+
+class FilePipeline:
+    def process_item(self, item: SeewaldepisodesItem, _):
+        with get(url=item.get("url"), stream=True) as r:
+            r.raise_for_status()
+            with open(item.get("url").split("/")[-1],"wb") as f:
+                data_chunk: bytes
+                for data_chunk in tqdm(r.iter_content(chunk_size=4096)):
+                    f.write(data_chunk)
         return item
